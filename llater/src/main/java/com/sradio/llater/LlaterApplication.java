a95fa18432a962e820bdd9aa1e4ed4959249dbd4
Ajeje <ajejebrazorf6453@gmail.com>
2018-07-14 10:38:22 +0200

    added llater module

---
+++ llater/src/main/java/com/sradio/llater/LlaterApplication.java
@@ -0,0 +1,12 @@
+package com.sradio.llater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlaterApplication.class, args);
+	}
+}
